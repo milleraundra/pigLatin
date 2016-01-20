62a72a93ebf33803c629ffb66da45e3baf35eebe
aundra miller and veronica alley <miller.aundra@gmail.com>
2016-01-20 15:35:20 -0800

    added qu functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var pigLatin = function(word) {
+
   if(word) {
     if (word.indexOf("a") === 0 || word.indexOf("e") === 0 || word.indexOf("o") === 0 || word.indexOf("i") === 0 || word.indexOf("u") === 0)  {
       // return "vowel";
@@ -6,22 +7,31 @@ var pigLatin = function(word) {
 
     }else if ((word.indexOf("a") === -1 && word.indexOf("e") === -1 && word.indexOf("o") === -1 && word.indexOf("i") === -1 && word.indexOf("u") === -1) && (word.indexOf("y") > -1)) {
         return word.concat("ay");
+      } else {
+          if (word.indexOf('qu') === -1) {
+            var re = /[aeiou]/;
+            var match = re.exec(word);
+            var vowelPosition = match.index;
 
-    }else {
-      // debugger;
-      var re = /[aeiou]/;
-      var match = re.exec(word);
-      var vowelPosition = match.index;
+            var slicedWord = word.slice(0, (vowelPosition));
+            var otherHalf = word.slice(vowelPosition, word.length);
+            return otherHalf + slicedWord + "ay";
 
-      var slicedWord = word.slice(0, (vowelPosition));
-      var otherHalf = word.slice(vowelPosition, word.length);
-      return otherHalf + slicedWord + "ay";
+          }else {
+            var secondRe = /u/;
+            var match = secondRe.exec(word);
+            var uPosition = match.index;
 
-    }
-  } else {
+            var slicedWord = word.slice(0, (uPosition + 1));
+            var otherHalf = word.slice(uPosition + 1, word.length);
+            return otherHalf + slicedWord + "ay";
+          }
+        }
+  }else {
     alert("Enter something");
   }
 }
+
 
 $(document).ready(function() {
   $("form#pigLatin").submit(function(event) {
