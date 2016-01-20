f1463d760d6f319f5912f5ecb36637cfa40e15ba
aundra miller and veronica alley <student@epicodus.com>
2016-01-20 13:06:25 -0800

    single word functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,47 @@
 var pigLatin = function(word) {
-
   if(word) {
-    if(word.startsWith("b")) {
-      return "consonant";
+    if (word.indexOf("a") === 0 || word.indexOf("e") === 0 || word.indexOf("o") === 0 || word.indexOf("i") === 0 || word.indexOf("u") === 0  ) {
+      // return "vowel";
+      return word.concat("ay");
     }else {
-      return "vowel";
+      // debugger;
+      var re = /[aeiou]/;
+      var match = re.exec(word);
+      var vowelPosition = match.index;
+
+      var slicedWord = word.slice(0, (vowelPosition));
+      var otherHalf = word.slice(vowelPosition, word.length);
+      return otherHalf + slicedWord + "ay";
+
     }
   } else {
     alert("Enter something");
   }
 }
+
+
+
+
+// var pigLatin = function(input) {
+//   if(input) {
+//
+//     var word = input.split(" ");
+//
+//     if (word.indexOf("a") === 0 || word.indexOf("e") === 0 || word.indexOf("o") === 0 || word.indexOf("i") === 0 || word.indexOf("u") === 0  ) {
+//       // return "vowel";
+//       return word.concat("ay");
+//     }else {
+//       // debugger;
+//       var re = /[aeiou]/;
+//       var match = re.exec(word);
+//       var vowelPosition = match.index;
+//
+//       var slicedWord = word.slice(0, (vowelPosition));
+//       var otherHalf = word.slice(vowelPosition, word.length);
+//       return otherHalf + slicedWord + "ay";
+//
+//     }
+//   } else {
+//     alert("Enter something");
+//   }
+// }
