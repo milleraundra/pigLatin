2cd7358a52ce6416adaa78cc5a798a498dfb5568
aundra miller and veronica alley <student@epicodus.com>
2016-01-20 14:51:11 -0800

    add full sentence functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 var pigLatin = function(word) {
   if(word) {
-    if (word.indexOf("a") === 0 || word.indexOf("e") === 0 || word.indexOf("o") === 0 || word.indexOf("i") === 0 || word.indexOf("u") === 0  ) {
+    if (word.indexOf("a") === 0 || word.indexOf("e") === 0 || word.indexOf("o") === 0 || word.indexOf("i") === 0 || word.indexOf("u") === 0)  {
       // return "vowel";
       return word.concat("ay");
+
+    }else if ((word.indexOf("a") === -1 && word.indexOf("e") === -1 && word.indexOf("o") === -1 && word.indexOf("i") === -1 && word.indexOf("u") === -1) && (word.indexOf("y") > -1)) {
+        return word.concat("ay");
+
     }else {
       // debugger;
       var re = /[aeiou]/;
@@ -19,8 +23,27 @@ var pigLatin = function(word) {
   }
 }
 
+$(document).ready(function() {
+  $("form#pigLatin").submit(function(event) {
+      // debugger;
+      var sentence = $("input#sentence").val();
+      sentence = sentence.toLowerCase().replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g,"");
+
+      var sentenceArray = sentence.split(" ");
+
+      var pigSentence = sentenceArray.map(pigLatin);
+
+      var finalSentence = pigSentence.join(" ");
 
 
+      // sentenceArray.forEach(pigLatin);
+
+      $(".translated").text(finalSentence)
+
+       event.preventDefault();
+  });
+});
+// var newSentence = sentence.toLowerCase();
 
 // var pigLatin = function(input) {
 //   if(input) {
@@ -45,3 +68,10 @@ var pigLatin = function(word) {
 //     alert("Enter something");
 //   }
 // }
+
+// for loop attempt 2
+
+
+      // for (var i = 0; i < sentenceArray.length; i += 1) {
+      //   var finalSentence = pigLatin(sentenceArray);
+      // }
