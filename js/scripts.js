c8ed40658e6f6e904718e36005cacb0e5bcc4a0f
aundra miller and veronica alley <student@epicodus.com>
2016-01-20 16:57:41 -0800

    final edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 var pigLatin = function(word) {
 
+
   if(word) {
-    if (word.indexOf("a") === 0 || word.indexOf("e") === 0 || word.indexOf("o") === 0 || word.indexOf("i") === 0 || word.indexOf("u") === 0)  {
+    if (word.indexOf("a") === 0 || word.indexOf("e") === 0 || word.indexOf("o") === 0 || word.indexOf("i") === 0 || word.indexOf("u") === 0) {
       // return "vowel";
       return word.concat("ay");
 
-    }else if ((word.indexOf("a") === -1 && word.indexOf("e") === -1 && word.indexOf("o") === -1 && word.indexOf("i") === -1 && word.indexOf("u") === -1) && (word.indexOf("y") > -1)) {
+    }else if ((word.indexOf("a") === -1 && word.indexOf("e") === -1 && word.indexOf("o") === -1 && word.indexOf("i") === -1 && word.indexOf("u") === -1) && (word.indexOf("y") > 0)) {
         return word.concat("ay");
       } else {
           if (word.indexOf('qu') === -1) {
@@ -53,35 +54,3 @@ $(document).ready(function() {
        event.preventDefault();
   });
 });
-// var newSentence = sentence.toLowerCase();
-
-// var pigLatin = function(input) {
-//   if(input) {
-//
-//     var word = input.split(" ");
-//
-//     if (word.indexOf("a") === 0 || word.indexOf("e") === 0 || word.indexOf("o") === 0 || word.indexOf("i") === 0 || word.indexOf("u") === 0  ) {
-//       // return "vowel";
-//       return word.concat("ay");
-//     }else {
-//       // debugger;
-//       var re = /[aeiou]/;
-//       var match = re.exec(word);
-//       var vowelPosition = match.index;
-//
-//       var slicedWord = word.slice(0, (vowelPosition));
-//       var otherHalf = word.slice(vowelPosition, word.length);
-//       return otherHalf + slicedWord + "ay";
-//
-//     }
-//   } else {
-//     alert("Enter something");
-//   }
-// }
-
-// for loop attempt 2
-
-
-      // for (var i = 0; i < sentenceArray.length; i += 1) {
-      //   var finalSentence = pigLatin(sentenceArray);
-      // }
