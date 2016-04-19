ff36c18c37a15e9822d25b0c0cbb31a0836719ef
Aundra Miller <miller.aundra@gmail.com>
2016-04-18 17:09:01 -0700

    update specs.js to test for words without vowels and words that are only vowels

--- specs/specs.js
+++ specs/specs.js
@@ -1,25 +1,11 @@
 describe('pigLatin', function() {
-  // it("reports if first letter is a consonant", function () {
-  //   expect(pigLatin("china")).to.equal("consonant");
-  // });
-  //
-  // it("reports if first letter is a vowel", function () {
-  //   expect(pigLatin("understand")).to.equal("vowel");
-  // });
-
-  // it("pigVowel should append 'ay' to the end of the vowel", function() {
-  //   expect(pigLatin("understand")).to.equal("understanday");
-  // });
-
-  // it("vowelPosition should tell index of first vowel", function() {
-  //   expect(pigLatin("buddy")).to.equal(0);
-  // });
-
   it("slicedWord and OtherHalf create new word", function() {
     expect(pigLatin("buddy")).to.equal("uddybay");
   });
-
-  it("takes a sentence and makes it an array", function() {
-    expect(pigLatin("hello there")).to.equals("ellohay erethay");
+  it("accounts for words with only vowels", function() {
+    expect(pigLatin("i")).to.equal("iay");
+  });
+  it("slicedWord and OtherHalf create new word", function() {
+    expect(pigLatin("gym")).to.equal("gymay");
   });
 });
